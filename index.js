8c4d77a4128b09b3b8acee0823ab119da2b86ace
zak-dev-art <zachndungu861@gmail.com>
2025-09-08 14:48:06 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
